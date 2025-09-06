865539922173a6ff5c51691f0cd36107c05155eb
KadaliSr <163870405+KadaliSr@users.noreply.github.com>
2025-09-06 16:56:11 +0530

    Update index.js to use environment variables and customer object

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,23 @@ let mongoose=require( 'mongoose')
 let rt =require( './routes/route.js')
 let bodyParser=require('body-parser')
 let cors=require("cors")
+require("dotenv").config();
+const myObject = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.MONGO_URI,      // match your .env
+  SECRET_KEY: process.env.JWT_SECRET  // match your .env
+};
+
+const customer = {
+  processENV: myObject
+};
+
+console.log("Customer object running in code:", customer);
+console.log("Port:", customer.processENV.PORT);
+console.log("Mongo URL:", customer.processENV.DB_URL);
+console.log("Secret Key:", customer.processENV.SECRET_KEY);
+
+
 mongoose.connect("mongodb://127.0.0.1:27017/project").then=()=>{
     console.log("ok")
 }
@@ -13,5 +30,9 @@ app.use('/prodimgs', express.static('prodimgs'))
 app.use(cors())
 app.use("/",rt)
 
-app.listen(5000)
+const PORT = customer.processENV.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
