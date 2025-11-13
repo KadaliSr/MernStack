7da317362e313f0d72b0171236c2bcd7aa0578f6
Srilekha Kadali <163870405+KadaliSr@users.noreply.github.com>
2025-11-13 17:17:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   // Serve index.html for all unknown routes
-  app.get("*", (req, res) =>
+  app.get("/*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
